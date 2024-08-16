925e8f8fd4d96c4bf483be6012da31a66b10e0a0
Felipe Felix <115411900+FelipeFelixhub@users.noreply.github.com>
2024-08-16 22:43:19 +0000

    update - index

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,35 @@
 import { Text, View, StyleSheet } from "react-native";
 
+const produtos = [
+  {id: 1, nome: "Coca-cola", preco: 5.5},
+  {id: 2, nome: "Pepsi", preco: 7.5},
+  {id: 3, nome: "Fanta", preco: 5.5},
+  {id: 4, nome: "Dolly", preco: 9.5}
+];
+
 export default function Index() {
   return (
     <View
-        style ={estilo.container}
+      style={estilo.container}
     >
-      <Text style={estilo.text}>Hello World!!</Text>
+      
     </View>
   );
 }
 
 const estilo = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "darkblue",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "darkblue",
 
   },
-  text:{
+  text: {
     color: "white",
     fontSize: 50,
     fontFamily: "sanserifa",
 
   }
-  
+
 });

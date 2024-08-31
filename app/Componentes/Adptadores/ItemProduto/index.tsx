3734a26e7d489ed31192c52822bf1f7d8521a6ba
Felipe Felix <115411900+FelipeFelixhub@users.noreply.github.com>
2024-08-31 00:12:14 +0000

    inerface proprod

--- app/Componentes/Adptadores/ItemProduto/index.tsx
+++ app/Componentes/Adptadores/ItemProduto/index.tsx
@@ -1,9 +1,15 @@
 import Produto from "@/app/Models/Produto";
 import Style from "@/app/Styles/Default";
+import React from "react";
 import { Text, View } from "react-native";
 
+interface PropProd{
+    produto:Produto,
+}
 
-function ItemProduto({produto}:{produto:Produto}){
+const ItemProduto: React.FC<PropProd> = ({produto})=>{
+
+    console.log(produto)
 return(
     <View style = {Style.card}>
         <Text style ={Style.cardText}>{produto.nome}</Text>
